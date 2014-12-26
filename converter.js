e3369d0e55c4d018a226e94c5bd48d21e8948d08
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-26 15:50:10 -0800

    adds initial math for chroma, secondary color & huePrime (C, X, H')

--- converter.js
+++ converter.js
@@ -2,6 +2,14 @@
 // expected saturation range: [0, 1]
 // expected lightness range: [0, 1]
 var hslToRgb = function(hue, saturation, lightness){
+  // based on algorithm from http://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
+  if( hue == undefined ){
+    return [0, 0, 0];
+  }
 
-  
+  var chroma = (1 - Math.abs(2 * lightness - 1)) * saturation;
+  var huePrime = hue / 60;
+  var secondComponent = chroma * (1 - Math.abs((huePrime % 2) - 1));
+
+
 };
